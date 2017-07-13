4476881dc57a8fd7fdbc9d7f4c5fe7f23c000463
LexPapaioannou <apapaioannou@luc.edu>
2017-07-12 20:41:52 -0500

    fixed typos in init file

--- initial.py
+++ initial.py
@@ -1,7 +1,12 @@
-import matplotlin
+#https://github.com/data-8/datascience
+
+import matplotlib
 matplotlib.use('Agg')
 from datascience import Table
 import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('fivethirtyeight')
+
+
+Table.read_table("~/Documents/Comp312/hw1/CDPH_Environmental_Inspections.csv")
 
